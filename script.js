8cf6b75e734e572a261c1961217673a3a0939ca9
kasthana112 <kanika.asthana@gmail.com>
2023-02-04 16:49:21 -0600

    updated errors

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ $(function () {
   var Today = dayjs();
 $('#currentDay').text(Today.format('MMM D, YYYY'));  
 })
+
+var hour = dayjs().format('H');
+
     for (let i = 9; i < 18; i++) {
       if (hour > i) {
         document.getElementById('h-' + i).classList.add('past');
@@ -13,4 +16,14 @@ $('#currentDay').text(Today.format('MMM D, YYYY'));
       }
     };
     
-    var hour = dayjs().format('H');
+$("#h-9 .description").val(localStorage.setItem("hour-9"));
+$("#h-10 .description").val(localStorage.setItem("hour-10"));
+$("#h-11 .description").val(localStorage.setItem("hour11"));
+$("#h-12 .description").val(localStorage.setItem("h-12"));
+$("#h-13 .description").val(localStorage.setItem("hour-13"));
+$("#h-14 .description").val(localStorage.setItem("hour-14"));
+$("#h-15 .description").val(localStorage.setItem("hour-15"));
+$("#h-16 .description").val(localStorage.setItem("hour-16"));
+$("#h-17 .description").val(localStorage.setItem("hour-17"));
+        
+      exactTime(0);
