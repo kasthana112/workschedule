74591435fd293ef11601609ed3bf5a845eeb6c4f
kasthana112 <kanika.asthana@gmail.com>
2023-02-04 17:46:33 -0600

    final draft

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ var hour = dayjs().format('H');
       }
     };
     
-$("#h-9 .description").val(localStorage.setItem("hour-9"));
-$("#h-10 .description").val(localStorage.setItem("hour-10"));
-$("#h-11 .description").val(localStorage.setItem("hour11"));
-$("#h-12 .description").val(localStorage.setItem("hour-12"));
-$("#h-13 .description").val(localStorage.setItem("hour-13"));
-$("#h-14 .description").val(localStorage.setItem("hour-14"));
-$("#h-15 .description").val(localStorage.setItem("hour-15"));
-$("#h-16 .description").val(localStorage.setItem("hour-16"));
-$("#h-17 .description").val(localStorage.setItem("hour-17"));
+//$("#h-9 .description").val(localStorage.setItem("hour-9"));
+//$("#h-10 .description").val(localStorage.setItem("hour-10"));
+//$("#h-11 .description").val(localStorage.setItem("hour11"));
+//$("#h-12 .description").val(localStorage.setItem("hour-12"));
+//$("#h-13 .description").val(localStorage.setItem("hour-13"));
+//$("#h-14 .description").val(localStorage.setItem("hour-14"));
+//$("#h-15 .description").val(localStorage.setItem("hour-15"));
+//$("#h-16 .description").val(localStorage.setItem("hour-16"));
+//$("#h-17 .description").val(localStorage.setItem("hour-17"));
         
-      exactTime(0);
+     // exactTime(0);//
